e0e3dd30ca572eeb1cd08e4056972f50fe2c1aed
dennisjosesilva <dennisjosesilva@gmail.com>
2014-11-15 00:48:45 -0200

    problem-9 step 2

--- problem9/main.py
+++ problem9/main.py
@@ -3,6 +3,26 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 
+def regression(xs, ys):
+
+	xmean = np.mean(xs)
+	ymean = np.mean(ys)
+
+	numerator = 0
+	denumerator = 0
+
+	for x, y in zip(xs, ys):
+		numerator += ((x - xmean) * (y - ymean))
+		denumerator += ((x - xmean) * (x - xmean))
+
+	w1 = numerator/denumerator
+	w0 = ymean - w1 * xmean
+
+	return w0, w1
+
+
+	
+
 n = 20.0
 step = 0.5
 a = 10.0
@@ -20,4 +40,11 @@ matplotlib.rcParams["axes.unicode_minus"] = False
 fig, ax = plt.subplots()
 ax.plot(xs, ys, "o")
 ax.set_title("problem 9")
+
 plt.savefig("problem9_1.png")
+
+w0, w1 = regression(xs, ys)
+
+ax.plot(xs, w0 + (w1*xs))
+
+plt.savefig("problem9_2.png")
